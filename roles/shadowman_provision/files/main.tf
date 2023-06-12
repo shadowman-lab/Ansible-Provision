8a5e6eea69da18bc2dff76d10240b7305c83c349
Alex Dworjan <adworjan@redhat.com>
2023-06-12 11:03:59 -0400

    change VM name

--- roles/shadowman_provision/files/main.tf
+++ roles/shadowman_provision/files/main.tf
@@ -37,7 +37,7 @@ data "vsphere_virtual_machine" "template8" {
 }
 
 resource "vsphere_virtual_machine" "server1" {
-  name             = "server15.shadowman.dev"
+  name             = "server14.shadowman.dev"
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
   folder           = "Discovered virtual machine"
